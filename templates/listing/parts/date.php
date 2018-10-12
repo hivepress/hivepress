d0a96835600415ca8d7e6ae1140efb33a3b06a4a
Ihor Ahnianikov <ihor@themex.co>
2018-10-12 10:53:34 +0200

    Fix dates

--- templates/listing/parts/date.php
+++ templates/listing/parts/date.php
@@ -2,4 +2,4 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<time class="hp-listing__date" datetime="<?php the_time( 'Y-m-d' ); ?>"><?php printf( esc_html__( 'Added on %s', 'hivepress' ), get_the_date() ); ?></time>
+<time class="hp-listing__date" datetime="<?php echo esc_attr( get_the_time( 'Y-m-d' ) ); ?>"><?php printf( esc_html__( 'Added on %s', 'hivepress' ), get_the_date() ); ?></time>
--- templates/vendor/parts/date.php
+++ templates/vendor/parts/date.php
@@ -2,4 +2,4 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<time class="hp-vendor__date" datetime="<?php echo date( 'Y-m-d', strtotime( $vendor->user_registered ) ); ?>"><?php printf( esc_html__( 'Member since %s', 'hivepress' ), date_i18n( get_option( 'date_format' ), strtotime( $vendor->user_registered ) ) ); ?></time>
+<time class="hp-vendor__date" datetime="<?php echo esc_attr( date( 'Y-m-d', strtotime( $vendor->user_registered ) ) ); ?>"><?php printf( esc_html__( 'Member since %s', 'hivepress' ), date_i18n( get_option( 'date_format' ), strtotime( $vendor->user_registered ) ) ); ?></time>
