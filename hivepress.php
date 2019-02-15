5410bbadf9ee7cec2d09da6d976bce8e39dfaf7f
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-02-15 23:26:30 +0100

    Add core class

---
+++ hivepress.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: HivePress
+ * Description: Multipurpose listing & directory plugin.
+ * Version: 1.1.0
+ * Author: HivePress
+ * Author URI: https://hivepress.io/
+ * Text Domain: hivepress
+ * Domain Path: /languages/
+ *
+ * @package HivePress
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
---
+++ includes/class-core.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * HivePress core.
+ *
+ * @package HivePress
+ */
+
+namespace HivePress;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * HivePress core class.
+ *
+ * @class Core
+ */
+final class Core {
+
+	/**
+	 * The single instance of the class.
+	 *
+	 * @var Core
+	 */
+	private static $instance;
+
+	// Forbid cloning and duplicating instances.
+	private function __clone() {}
+	private function __wakeup() {}
+
+	/**
+	 * Ensures only one plugin instance is loaded.
+	 *
+	 * @see hivepress()
+	 * @return Core
+	 */
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+}
