73a002f8b31e156ef798529858ea57e87a7d10e5
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-02-20 17:18:35 +0100

    Add editor styles

--- includes/components/class-editor.php
+++ includes/components/class-editor.php
@@ -21,6 +21,19 @@ final class Editor {
 	 * Class constructor.
 	 */
 	public function __construct() {
+		if ( is_admin() ) {
+			add_action( 'admin_init', [ $this, 'enqueue_styles' ] );
+		}
+	}
 
+	/**
+	 * Enqueues styles.
+	 */
+	public function enqueue_styles() {
+		foreach ( hivepress()->get_config( 'styles' ) as $style ) {
+			if ( hp_get_array_value( $style, 'editor', false ) ) {
+				add_editor_style( $style['src'] );
+			}
+		}
 	}
 }
