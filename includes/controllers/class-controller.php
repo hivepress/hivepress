0e4c9af5a33f18eee21b600ab53f15e9bae02b06
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-03-03 22:06:24 +0100

    Cleanup controllers

---
+++ includes/controllers/class-controller.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Abstract controller.
+ *
+ * @package HivePress\Controllers
+ */
+
+namespace HivePress\Controllers;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Abstract controller class.
+ *
+ * @class Controller
+ */
+abstract class Controller {
+
+	/**
+	 * Controller name.
+	 *
+	 * @var string
+	 */
+	protected $name;
+
+	/**
+	 * Controller routes.
+	 *
+	 * @var array
+	 */
+	protected $routes = [];
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $args Controller arguments.
+	 */
+	public function __construct( $args = [] ) {
+
+		// Set name.
+		$args['name'] = strtolower( ( new \ReflectionClass( $this ) )->getShortName() );
+
+		// Set properties.
+		foreach ( $args as $name => $value ) {
+			$this->set_property( $name, $value );
+		}
+	}
+
+	/**
+	 * Sets property.
+	 *
+	 * @param string $name Property name.
+	 * @param mixed  $value Property value.
+	 */
+	final protected function set_property( $name, $value ) {
+		if ( property_exists( $this, $name ) ) {
+			if ( method_exists( $this, 'set_' . $name ) ) {
+				$this->$name = call_user_func_array( [ $this, 'set_' . $name ], [ $value ] );
+			} else {
+				$this->$name = $value;
+			}
+		}
+	}
+
+	/**
+	 * Gets controller name.
+	 *
+	 * @return string
+	 */
+	final public function get_name() {
+		return $this->name;
+	}
+
+	/**
+	 * Gets controller routes.
+	 *
+	 * @return array
+	 */
+	final public function get_routes() {
+		return $this->routes;
+	}
+}
---
+++ includes/controllers/class-listing.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * Listing controller.
+ *
+ * @package HivePress\Controllers
+ */
+
+namespace HivePress\Controllers;
+
+use HivePress\Models as Models;
+use HivePress\Forms as Forms;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Listing controller class.
+ *
+ * @class Listing
+ */
+class Listing extends Controller {
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $args Controller arguments.
+	 */
+	public function __construct( $args = [] ) {
+		$args = array_replace_recursive(
+			$args,
+			[
+				'routes' => [
+					[
+						'path'      => '/listings',
+						'rest'      => true,
+						'endpoints' => [
+							[
+								'path'    => '/(?P<id>\d+)',
+								'methods' => 'POST',
+								'action'  => 'update_listing',
+							],
+
+							[
+								'path'    => '/(?P<id>\d+)',
+								'methods' => 'DELETE',
+								'action'  => 'delete_listing',
+							],
+						],
+					],
+
+					[
+						'rule'   => 'is_listing_page',
+						'action' => 'render_listing_page',
+					],
+
+					[
+						'rule'   => 'is_listings_page',
+						'action' => 'render_listings_page',
+					],
+				],
+			]
+		);
+
+		parent::__construct( $args );
+	}
+
+	/**
+	 * Updates listing.
+	 *
+	 * @param WP_REST_Request $request API request.
+	 * @return WP_Rest_Response
+	 */
+	public function update_listing( $request ) {
+
+		// Check authentication.
+		if ( ! is_user_logged_in() ) {
+			return hp_rest_error( 401 );
+		}
+
+		// Get listing.
+		$listing = Models\Listing::get( $request->get_param( 'id' ) );
+
+		if ( is_null( $listing ) ) {
+			return hp_rest_error( 404 );
+		}
+
+		// Check permissions.
+		// todo add author_id and status fields to model.
+		if ( ! current_user_can( 'edit_others_posts' ) && ( get_current_user_id() !== $listing->get_author_id() || ! in_array( $listing->get_status(), [ 'auto-draft', 'draft', 'publish' ], true ) ) ) {
+			return hp_rest_error( 403 );
+		}
+
+		// Validate form.
+		$form = new Forms\Listing_Update();
+
+		$form->set_values( $request->get_params() );
+
+		if ( ! $form->validate() ) {
+			return hp_rest_error( 400, $form->get_errors() );
+		}
+
+		// Update listing.
+		$listing->fill( $form->get_values() );
+
+		if ( ! $listing->save() ) {
+			return hp_rest_error( 400, esc_html__( 'Error updating listing', 'hivepress' ) );
+		}
+
+		return new \WP_Rest_Response(
+			[
+				'data' => [
+					'id' => $listing->get_id(),
+				],
+			],
+			200
+		);
+	}
+
+	/**
+	 * Deletes listing.
+	 *
+	 * @param WP_REST_Request $request API request.
+	 * @return WP_Rest_Response
+	 */
+	public function delete_listing( $request ) {
+
+		// Check authentication.
+		if ( ! is_user_logged_in() ) {
+			return hp_rest_error( 401 );
+		}
+
+		// Get listing.
+		$listing = Models\Listing::get( $request->get_param( 'id' ) );
+
+		if ( is_null( $listing ) ) {
+			return hp_rest_error( 404 );
+		}
+
+		// Check permissions.
+		// todo add fields to model.
+		if ( ! current_user_can( 'delete_others_posts' ) && ( get_current_user_id() !== $listing->get_author_id() || ! in_array( $listing->get_status(), [ 'auto-draft', 'draft', 'publish' ], true ) ) ) {
+			return hp_rest_error( 403 );
+		}
+
+		// Delete listing.
+		if ( ! $listing->delete() ) {
+			return hp_rest_error( 400, esc_html__( 'Error deleting listing', 'hivepress' ) );
+		}
+
+		return new \WP_Rest_Response( (object) [], 204 );
+	}
+
+	/**
+	 * Checks listing page.
+	 *
+	 * @return bool
+	 */
+	public function is_listing_page() {
+		return is_singular( 'hp_listing' );
+	}
+
+	/**
+	 * Renders listing page.
+	 *
+	 * @return string
+	 */
+	public function render_listing_page() {
+		// todo.
+	}
+
+	/**
+	 * Checks listings page.
+	 *
+	 * @return bool
+	 */
+	public function is_listings_page() {
+		return is_page( absint( get_option( 'hp_page_listings' ) ) ) || is_post_type_archive( 'hp_listing' ) || is_tax( get_object_taxonomies( 'hp_listing' ) );
+	}
+
+	/**
+	 * Renders listings page.
+	 *
+	 * @return string
+	 */
+	public function render_listings_page() {
+		// todo.
+		$output = '';
+
+		ob_start();
+		get_header();
+		$output .= ob_get_contents();
+		ob_end_clean();
+
+		$template = hivepress()->get_config( 'templates' )['listings'];
+
+		foreach ( $template['blocks'] as $block_name => $block ) {
+			$block_class = '\HivePress\Blocks\\' . $block['type'];
+
+			$output .= ( new $block_class( $block ) )->render();
+		}
+
+		ob_start();
+		get_footer();
+		$output .= ob_get_contents();
+		ob_end_clean();
+
+		return $output;
+	}
+}
