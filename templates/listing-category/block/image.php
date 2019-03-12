99e3ba35dee1435b7ffba9c60720ee83eb4a42ca
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-03-12 12:28:43 +0100

    Fix image placeholders

--- templates/listing-category/block/image.php
+++ templates/listing-category/block/image.php
@@ -7,7 +7,7 @@ defined( 'ABSPATH' ) || exit;
 		<?php if ( $category->get_image_id() ) : ?>
 			<img src="<?php echo esc_url( $category->get_image_url( 'hp_landscape_small' ) ); ?>" alt="<?php echo esc_attr( $category->get_name() ); ?>">
 		<?php else : ?>
-			<img src="<?php echo esc_url( HP_CORE_URL . '/assets/images/placeholder.png' ); ?>" alt="<?php echo esc_attr( $category->get_name() ); ?>">
+			<img src="<?php echo esc_url( HP_CORE_URL . '/assets/images/placeholders/image-landscape.svg' ); ?>" alt="<?php echo esc_attr( $category->get_name() ); ?>">
 		<?php endif; ?>
 	</a>
 </div>
