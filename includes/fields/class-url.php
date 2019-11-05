9abc934610b7315f946b6f82d24eb3ed4d1f012f
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-05 14:56:46 +0200

    Add URL attribute type

---
+++ includes/fields/class-url.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * URL field.
+ *
+ * @package HivePress\Fields
+ */
+
+namespace HivePress\Fields;
+
+use HivePress\Helpers as hp;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * URL field class.
+ *
+ * @class URL
+ */
+class URL extends Text {
+
+	/**
+	 * Field type.
+	 *
+	 * @var string
+	 */
+	protected static $type;
+
+	/**
+	 * Field title.
+	 *
+	 * @var string
+	 */
+	protected static $title;
+
+	/**
+	 * Field settings.
+	 *
+	 * @var array
+	 */
+	protected static $settings = [];
+
+	/**
+	 * Class initializer.
+	 *
+	 * @param array $args Field arguments.
+	 */
+	public static function init( $args = [] ) {
+		$args = hp\merge_arrays(
+			[
+				'title'    => esc_html__( 'URL', 'hivepress' ),
+
+				'settings' => [
+					'min_length' => null,
+					'max_length' => null,
+				],
+			],
+			$args
+		);
+
+		parent::init( $args );
+	}
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $args Field arguments.
+	 */
+	public function __construct( $args = [] ) {
+
+		// Set maximum length.
+		$args['max_length'] = 2048;
+
+		parent::__construct( $args );
+	}
+
+	/**
+	 * Sanitizes field value.
+	 */
+	protected function sanitize() {
+		if ( ! is_null( $this->value ) ) {
+			$this->value = esc_url_raw( $this->value );
+		}
+	}
+
+	/**
+	 * Renders field HTML.
+	 *
+	 * @return string
+	 */
+	public function render() {
+		return '<input type="' . esc_attr( static::$type ) . '" name="' . esc_attr( $this->name ) . '" value="' . esc_url( $this->value ) . '" ' . hp\html_attributes( $this->attributes ) . '>';
+	}
+}
