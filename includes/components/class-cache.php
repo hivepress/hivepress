2551a174a9b2ce4f4f207ca843e67b35939a8b36
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-07 00:20:34 +0200

    Fix caching

--- includes/components/class-cache.php
+++ includes/components/class-cache.php
@@ -27,13 +27,15 @@ final class Cache {
 		// Schedule events.
 		add_action( 'hivepress/v1/activate', [ $this, 'schedule_events' ] );
 
-		// Clear cache.
-		add_action( 'save_post', [ $this, 'clear_post_cache' ] );
-		add_action( 'delete_post', [ $this, 'clear_post_cache' ] );
+		// Delete cache.
+		add_action( 'hivepress/v1/cron/daily', [ $this, 'delete_expired_cache' ] );
 
-		add_action( 'create_term', [ $this, 'clear_term_cache' ], 10, 3 );
-		add_action( 'edit_term', [ $this, 'clear_term_cache' ], 10, 3 );
-		add_action( 'delete_term', [ $this, 'clear_term_cache' ], 10, 3 );
+		add_action( 'save_post', [ $this, 'delete_post_cache' ] );
+		add_action( 'delete_post', [ $this, 'delete_post_cache' ] );
+
+		add_action( 'create_term', [ $this, 'delete_term_cache' ], 10, 3 );
+		add_action( 'edit_term', [ $this, 'delete_term_cache' ], 10, 3 );
+		add_action( 'delete_term', [ $this, 'delete_term_cache' ], 10, 3 );
 	}
 
 	/**
@@ -50,60 +52,50 @@ final class Cache {
 	}
 
 	/**
-	 * Clears post cache.
-	 *
-	 * @param int $post_id Post ID.
-	 */
-	public function clear_post_cache( $post_id ) {
-		$post_type = get_post_type( $post_id );
-
-		if ( in_array( $post_type, hp\prefix( array_keys( hivepress()->get_config( 'post_types' ) ) ), true ) ) {
-			$this->clear_cache( hp\unprefix( $post_type ) . '/*' );
-		}
-	}
-
-	/**
-	 * Clears term cache.
-	 *
-	 * @param int    $term_id Term ID.
-	 * @param int    $term_taxonomy_id Term taxonomy ID.
-	 * @param string $taxonomy Taxonomy name.
-	 */
-	public function clear_term_cache( $term_id, $term_taxonomy_id, $taxonomy ) {
-		if ( in_array( $taxonomy, hp\prefix( array_keys( hivepress()->get_config( 'taxonomies' ) ) ), true ) ) {
-			$this->clear_cache( hp\unprefix( $taxonomy ) . '/*' );
-		}
-	}
-
-	/**
 	 * Gets cache.
 	 *
 	 * @param mixed $names Cache names.
+	 * @param bool  $expire Expiration check.
 	 * @return mixed
 	 */
-	public function get_cache( $names ) {
+	public function get_cache( $names, $expire = true ) {
 		$cache = null;
 
-		// Get meta value.
-		$id = null;
+		// Get cache name.
+		$name = $this->get_cache_name( $names );
 
-		if ( is_array( $names ) && count( $names ) > 2 ) {
-			$id = reset( $names );
+		// Get cache ID.
+		$id = $this->get_cache_id( $names );
 
-			if ( is_numeric( $id ) ) {
-				array_shift( $names );
+		if ( ! empty( $id ) ) {
 
-				$callback = 'get_' . array_shift( $names ) . '_meta';
+			// Get meta value.
+			$type     = $names[1];
+			$callback = 'get_' . $type . '_meta';
 
-				if ( function_exists( $callback ) ) {
-					$cache = call_user_func_array( $callback, [ $id, $this->get_cache_name( $names ), true ] );
+			if ( function_exists( $callback ) ) {
+
+				// Get timeout.
+				$timeout = 0;
+
+				if ( $expire ) {
+					$timeout = absint( call_user_func_array( $callback, [ $id, '_transient_timeout_' . $name, true ] ) );
+				}
+
+				if ( 0 !== $timeout && $timeout <= time() ) {
+
+					// Delete value.
+					$this->delete_cache( [ $id, $type, hp\unprefix( $name ) ] );
+				} else {
+
+					// Get value.
+					$cache = call_user_func_array( $callback, [ $id, '_transient_' . $name, true ] );
 				}
 			}
-		}
+		} else {
 
-		// Get transient value.
-		if ( ! is_numeric( $id ) ) {
-			$cache = get_transient( $this->get_cache_name( $names ) );
+			// Get transient value.
+			$cache = get_transient( $name );
 		}
 
 		// Normalize value.
@@ -119,58 +111,163 @@ final class Cache {
 	 *
 	 * @param mixed $names Cache names.
 	 * @param mixed $value Cache value.
-	 * @param int   $expiration Expiration period.
+	 * @param int   $timeout Expiration timeout.
 	 */
-	public function set_cache( $names, $value, $expiration = 0 ) {
+	public function set_cache( $names, $value, $timeout = 0 ) {
 
-		// Set meta value.
-		$id = null;
+		// Get cache name.
+		$name = $this->get_cache_name( $names );
 
-		if ( is_array( $names ) && count( $names ) > 2 ) {
-			$id = reset( $names );
+		// Get cache ID.
+		$id = $this->get_cache_id( $names );
 
-			if ( is_numeric( $id ) ) {
-				array_shift( $names );
+		if ( ! empty( $id ) ) {
 
-				$callback = 'update_' . array_shift( $names ) . '_meta';
+			// Set meta value.
+			$type     = $names[1];
+			$callback = 'update_' . $type . '_meta';
 
-				if ( function_exists( $callback ) ) {
-					$cache = call_user_func_array( $callback, [ $id, $this->get_cache_name( $names ), $value ] );
+			if ( function_exists( $callback ) ) {
+
+				// Set value.
+				call_user_func_array( $callback, [ $id, '_transient_' . $name, $value ] );
+
+				// Set timeout.
+				if ( $timeout > 0 ) {
+					call_user_func_array( $callback, [ $id, '_transient_timeout_' . $name, time() + $timeout ] );
 				}
 			}
-		}
+		} else {
 
-		// Set transient value.
-		if ( ! is_numeric( $id ) ) {
-			set_transient( $this->get_cache_name( $names ), $value, $expiration );
+			// Set transient value.
+			set_transient( $name, $value, $timeout );
 		}
 	}
 
 	/**
-	 * Clears cache.
+	 * Deletes cache.
 	 *
-	 * @param mixed $name Cache name.
+	 * @param mixed $names Cache names.
+	 * @param bool  $expire Expiration check.
 	 */
-	public function clear_cache( $name ) {
+	public function delete_cache( $names, $expire = true ) {
+		if ( is_array( $names ) && end( $names ) === '*' ) {
+			array_pop( $names );
+
+			// Update cache version.
+			$this->update_cache_version( $names );
+		} else {
+
+			// Get cache name.
+			$name = $this->get_cache_name( $names );
+
+			// Get cache ID.
+			$id = $this->get_cache_id( $names );
+
+			if ( ! empty( $id ) ) {
+
+				// Delete meta value.
+				$type     = $names[1];
+				$callback = 'delete_' . $type . '_meta';
+
+				if ( function_exists( $callback ) ) {
+					call_user_func_array( $callback, [ $id, '_transient_' . $name ] );
+
+					if ( $expire ) {
+						call_user_func_array( $callback, [ $id, '_transient_timeout_' . $name ] );
+					}
+				}
+			} else {
+
+				// Delete transient.
+				delete_transient( $name );
+			}
+		}
+	}
+
+	/**
+	 * Deletes expired cache.
+	 */
+	public function delete_expired_cache() {
 		global $wpdb;
 
-		if ( strpos( $name, '*' ) !== false ) {
+		// Set meta types.
+		$types = [ 'user', 'post', 'term', 'comment' ];
 
-			// Get transients.
-			$transients = $wpdb->get_col(
-				$wpdb->prepare(
-					"SELECT option_name FROM $wpdb->options WHERE option_name LIKE %s",
-					'_transient_' . hp\prefix( str_replace( '*', '%', str_replace( '_', '\_', $name ) ) )
-				)
-			);
+		foreach ( $types as $type ) {
+			$callback = 'delete_' . $type . '_meta';
+
+			if ( function_exists( $callback ) ) {
+
+				// Get meta values.
+				$meta_values = $wpdb->get_results(
+					$wpdb->prepare(
+						'SELECT %s, meta_key FROM %s WHERE meta_key LIKE %s AND CAST(meta_value AS SIGNED) <= %s',
+						$type . '_id',
+						$wpdb->prefix . $type . 'meta',
+						'\_transient\_timeout\_%',
+						time()
+					),
+					ARRAY_A
+				);
+
+				// Delete meta values.
+				if ( ! empty( $meta_values ) ) {
+					foreach ( $meta_values as $meta_value ) {
+						call_user_func_array( $callback, [ $meta_value[ $type . '_id' ], $meta_value[ $meta_key ] ] );
+						call_user_func_array( $callback, [ $meta_value[ $type . '_id' ], preg_replace( '/^_transient_timeout/', '_transient', $meta_value[ $meta_key ] ) ] );
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Deletes post cache.
+	 *
+	 * @param int $post_id Post ID.
+	 */
+	public function delete_post_cache( $post_id ) {
+		if ( in_array( get_post_type( $post_id ), hp\prefix( array_keys( hivepress()->get_config( 'post_types' ) ) ), true ) ) {
+
+			// Get post.
+			$post = get_post( $post_id );
 
 			// Delete transients.
-			foreach ( $transients as $transient ) {
-				delete_transient( substr( $transient, strlen( '_transient_' ) ) );
-			}
-		} else {
-			delete_transient( hp\prefix( $name ) );
+			$this->delete_cache( [ hp\unprefix( $post->post_type ), '*' ] );
+
+			// Delete meta.
+			$this->delete_cache( [ absint( $post->post_author ), 'user', hp\unprefix( $post->post_type ), '*' ] );
 		}
+	}
+
+	/**
+	 * Deletes term cache.
+	 *
+	 * @param int    $term_id Term ID.
+	 * @param int    $term_taxonomy_id Term taxonomy ID.
+	 * @param string $taxonomy Taxonomy name.
+	 */
+	public function delete_term_cache( $term_id, $term_taxonomy_id, $taxonomy ) {
+		if ( in_array( $taxonomy, hp\prefix( array_keys( hivepress()->get_config( 'taxonomies' ) ) ), true ) ) {
+			$this->delete_cache( [ hp\unprefix( $taxonomy ), '*' ] );
+		}
+	}
+
+	/**
+	 * Gets cache ID.
+	 *
+	 * @param mixed $names Cache names.
+	 * @return int
+	 */
+	protected function get_cache_id( $names ) {
+		$id = null;
+
+		if ( is_array( $names ) && count( $names ) >= 3 ) {
+			$id = absint( reset( $names ) );
+		}
+
+		return $id;
 	}
 
 	/**
@@ -183,21 +280,84 @@ final class Cache {
 		$name = $names;
 
 		if ( is_array( $names ) ) {
-			$name = '';
 
-			ksort( $names );
+			// Get cache ID.
+			$id   = $this->get_cache_id( $names );
+			$type = null;
+
+			if ( ! empty( $id ) ) {
+				array_shift( $names );
+
+				$type = array_shift( $names );
+			}
+
+			// Get cache version.
+			$version = '';
 
 			foreach ( $names as $part ) {
 				if ( is_array( $part ) ) {
-					$part = md5( wp_json_encode( $part ) );
+					if ( ! empty( $id ) ) {
+						$version = $this->get_cache_version( [ $id, $type, reset( $names ) ] );
+					} else {
+						$version = $this->get_cache_version( reset( $names ) );
+					}
+
+					break;
+				}
+			}
+
+			// Get cache name.
+			$name = array_shift( $names );
+
+			foreach ( $names as $part ) {
+				if ( is_array( $part ) ) {
+					ksort( $part );
+
+					$part = md5( wp_json_encode( $part ) . $version );
 				}
 
 				$name .= '/' . strval( $part );
 			}
-
-			$name = ltrim( $name, '/' );
 		}
 
 		return hp\prefix( $name );
+	}
+
+	/**
+	 * Gets cache version.
+	 *
+	 * @param mixed $names Cache names.
+	 * @return string
+	 */
+	protected function get_cache_version( $names ) {
+		$version = $this->get_cache( array_merge( (array) $names, 'version' ), false );
+
+		if ( empty( $version ) ) {
+			$version = $this->update_cache_version( $names );
+		}
+
+		return $version;
+	}
+
+	/**
+	 * Updates cache version.
+	 *
+	 * @param mixed $names Cache names.
+	 * @return string
+	 */
+	protected function update_cache_version( $names ) {
+
+		// Get cache names.
+		$name = array_merge( (array) $names, 'version' );
+
+		// Delete old versions.
+		$this->delete_cache( $name, false );
+
+		// Set new version.
+		$version = (string) time();
+
+		$this->set_cache( $name, $version );
+
+		return $version;
 	}
 }
