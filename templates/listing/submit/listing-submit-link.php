abd80e38803f4252e5420bb34907cca7806aa422
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-08 21:59:49 +0200

    Change text

--- templates/listing/submit/listing-submit-link.php
+++ templates/listing/submit/listing-submit-link.php
@@ -4,6 +4,6 @@ defined( 'ABSPATH' ) || exit;
 
 if ( get_option( 'hp_listing_enable_submission' ) ) :
 	?>
-	<button type="button" class="hp-menu__item button" data-component="link" data-url="<?php echo esc_url( hivepress()->router->get_url( 'listing/submit_listing' ) ); ?>"><?php esc_html_e( 'Submit Listing', 'hivepress' ); ?></button>
+	<button type="button" class="hp-menu__item button" data-component="link" data-url="<?php echo esc_url( hivepress()->router->get_url( 'listing/submit_listing' ) ); ?>"><?php esc_html_e( 'Add Listing', 'hivepress' ); ?></button>
 	<?php
 endif;
