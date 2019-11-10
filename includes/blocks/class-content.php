6dfe15e18fdcf86278e409c1a2a54ea5ee991cab
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-10 19:46:02 +0200

    Add content block type

---
+++ includes/blocks/class-content.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Content block.
+ *
+ * @package HivePress\Blocks
+ */
+
+namespace HivePress\Blocks;
+
+use HivePress\Helpers as hp;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Content block class.
+ *
+ * @class Content
+ */
+class Content extends Block {
+
+	/**
+	 * Block type.
+	 *
+	 * @var string
+	 */
+	protected static $type;
+
+	/**
+	 * Block content.
+	 *
+	 * @var string
+	 */
+	protected $content = '';
+
+	/**
+	 * Renders block HTML.
+	 *
+	 * @return string
+	 */
+	public function render() {
+		return $this->content;
+	}
+}
