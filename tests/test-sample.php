9cb1522a66d4de06a0d143ab000e6561c3608bf9
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-13 21:46:31 +0200

    Fix test

---
+++ tests/test-sample.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wp_Meta_Verify
+ */
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( false );
+	}
+}
