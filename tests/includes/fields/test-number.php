ce1ee41702347f78b6d2e6adbe854c6953273d4a
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-13 22:02:11 +0200

    Add number field test

---
+++ tests/includes/fields/test-number.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Number field test.
+ *
+ * @package HivePress\Tests\Fields
+ */
+
+namespace HivePress\Tests\Fields;
+
+use HivePress\Fields;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Number field class test.
+ *
+ * @class Number
+ */
+class Number extends WP_UnitTestCase {
+	public function test_todo() {
+		$this->assertTrue( false );
+	}
+}
