505b1732670b8d375e6a1d6079635ca977f09743
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-14 16:59:41 +0200

    Fix tests

--- tests/includes/components/class-cache.php
+++ tests/includes/components/class-cache.php
@@ -20,6 +20,24 @@ class Cache extends \PHPUnit\Framework\TestCase {
 	 * @test
 	 */
 	public function get_cache() {
-		$this->assertTrue( false );
+
+		// Get non-existent.
+		$this->assertSame( null, hivepress()->cache->get_cache( 'key' ) );
+
+		// Get by key.
+		set_transient( 'hp_key', 'value' );
+
+		$this->assertSame( 'value', hivepress()->cache->get_cache( 'key' ) );
+
+		// Get by key and group.
+		$version = (string) time();
+
+		set_transient( 'hp_group/version', $version );
+
+		set_transient( 'hp_group/' . md5( 'key' . $version ), 'value' );
+		set_transient( 'hp_group/' . md5( wp_json_encode( [ 'key1', 'key2' ] ) . $version ), 'value' );
+
+		$this->assertSame( 'value', hivepress()->cache->get_cache( 'key', 'group' ) );
+		$this->assertSame( 'value', hivepress()->cache->get_cache( [ 'key2', 'key1' ], 'group' ) );
 	}
 }
