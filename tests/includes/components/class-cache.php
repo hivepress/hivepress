308c3086c23611aa265fc5642cf9e3d972039fcc
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-14 16:28:37 +0200

    Add cache test

---
+++ tests/includes/components/class-cache.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Cache test component.
+ *
+ * @package HivePress\Tests\Components
+ */
+
+namespace HivePress\Tests\Components;
+
+/**
+ * Cache component test class.
+ *
+ * @class Cache
+ */
+class Cache extends PHPUnit\Framework\TestCase {
+
+	/**
+	 * Gets transient cache.
+	 *
+	 * @test
+	 */
+	public function get_cache() {
+		$this->assertTrue(false);
+	}
+}
