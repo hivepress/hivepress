55df89376451cd7549d930d5d8207e281bdb5a11
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-17 13:43:38 +0200

    Fix tests

--- tests/includes/fields/class-email.php
+++ tests/includes/fields/class-email.php
@@ -17,17 +17,29 @@ use HivePress\Fields;
 class Email extends \PHPUnit\Framework\TestCase {
 
 	/**
+	 * Field object.
+	 *
+	 * @var object
+	 */
+	protected $field;
+
+	/**
+	 * Setups test.
+	 */
+	protected function setUp() {
+		$this->field = new Fields\Email();
+	}
+
+	/**
 	 * Sanitizes field value.
 	 *
 	 * @test
 	 */
 	public function sanitize() {
-		$field = new Fields\Email();
+		$value = 'a"b(c)d,e:f;gi[j\k]l@example.com';
 
-		$email = 'a"b(c)d,e:f;gi[j\k]l@example.com';
-
-		$field->set_value( $email );
-		$this->assertSame( sanitize_email( $email ), $field->get_value() );
+		$this->field->set_value( $value );
+		$this->assertSame( sanitize_email( $value ), $this->field->get_value() );
 	}
 
 	/**
@@ -36,18 +48,16 @@ class Email extends \PHPUnit\Framework\TestCase {
 	 * @test
 	 */
 	public function validate() {
-		$field = new Fields\Email();
+		$this->field->set_value( null );
+		$this->assertTrue( $this->field->validate() );
 
-		$field->set_value( null );
-		$this->assertTrue( $field->validate() );
+		$this->field->set_value( 'example@example.com' );
+		$this->assertTrue( $this->field->validate() );
 
-		$field->set_value( 'example@example.com' );
-		$this->assertTrue( $field->validate() );
+		$this->field->set_value( 'example@example' );
+		$this->assertFalse( $this->field->validate() );
 
-		$field->set_value( 'example@example' );
-		$this->assertFalse( $field->validate() );
-
-		$field->set_value( 'eqbujptxfckdjykzckylzqfgmaalxsnknoukcoajrmwjnbptsuuvumyfvgilpivpoevotkzohqvjgkhdaxqibzxmtapxdnairomksiyqxsnuhlognrcoabkgfncorlgrvufcthrciuvkgpvuztfvjxfpvwxhrozcswnrcotcbnumkvfwqnieefwepsibmvqzvwjbcamrxwfwtfydokcxtcisllaezgvjaaupftsfyvhipocaumf@example.com' );
-		$this->assertFalse( $field->validate() );
+		$this->field->set_value( 'eqbujptxfckdjykzckylzqfgmaalxsnknoukcoajrmwjnbptsuuvumyfvgilpivpoevotkzohqvjgkhdaxqibzxmtapxdnairomksiyqxsnuhlognrcoabkgfncorlgrvufcthrciuvkgpvuztfvjxfpvwxhrozcswnrcotcbnumkvfwqnieefwepsibmvqzvwjbcamrxwfwtfydokcxtcisllaezgvjaaupftsfyvhipocaumf@example.com' );
+		$this->assertFalse( $this->field->validate() );
 	}
 }
---
+++ tests/includes/fields/class-number-range.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Number range field test.
+ *
+ * @package HivePress\Tests\Fields
+ */
+
+namespace HivePress\Tests\Fields;
+
+use HivePress\Fields;
+
+/**
+ * Number range field test class.
+ *
+ * @class Number_Range
+ */
+class Number_Range extends \PHPUnit\Framework\TestCase {
+
+	/**
+	 * Field object.
+	 *
+	 * @var object
+	 */
+	protected $field;
+
+	/**
+	 * Setups test.
+	 */
+	protected function setUp() {
+		$this->field = new Fields\Number_Range();
+	}
+
+	/**
+	 * Normalizes field value.
+	 *
+	 * @test
+	 */
+	public function normalize() {
+		$values = [ true, 1, 'value', [], [ 1 ], [ 1, 2, 3 ], new stdClass() ];
+
+		foreach ( $values as $value ) {
+			$this->field->set_value( $value );
+			$this->assertSame( null, $this->field->get_value() );
+		}
+
+		$this->field->set_value( [ 2, 1 ] );
+		$this->assertSame( [ 1, 2 ], $this->field->get_value() );
+	}
+
+	/**
+	 * Sanitizes field value.
+	 *
+	 * @test
+	 */
+	public function sanitize() {
+		$values = [ [ null, null ], [ 'a', 'b' ], [ 1, 'b' ], [ 'a', 2 ] ];
+
+		foreach ( $values as $value ) {
+			$this->field->set_value( $value );
+			$this->assertSame( null, $this->field->get_value() );
+		}
+
+		$this->field->set_value( [ 0, 0 ] );
+		$this->assertSame( [ 0, 0 ], $this->field->get_value() );
+	}
+
+	/**
+	 * Validates field value.
+	 *
+	 * @test
+	 */
+	public function validate() {
+		$this->field->set_value( null );
+		$this->assertTrue( $this->field->validate() );
+
+		$this->field->set_value( [ 2, 1 ] );
+		$this->assertTrue( $this->field->validate() );
+		$this->assertSame( [ 1, 2 ], $this->field->get_value() );
+	}
+}
