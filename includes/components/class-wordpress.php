d14fd2943d575564ca9ef615155ee94cbc7975fc
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-22 21:43:14 +0200

    Fix action aliases

--- includes/components/class-wordpress.php
+++ includes/components/class-wordpress.php
@@ -24,9 +24,16 @@ final class WordPress {
 	 */
 	public function __construct() {
 
+		// Update users.
+		add_action( 'user_register', [ $this, 'update_user' ] );
+		add_action( 'profile_update', [ $this, 'update_user' ] );
+		add_action( 'delete_user', [ $this, 'update_user' ] );
+
 		// Update posts.
 		add_action( 'save_post', [ $this, 'update_post' ], 10, 3 );
 		add_action( 'delete_post', [ $this, 'update_post' ] );
+
+		add_action( 'transition_post_status', [ $this, 'update_post_status' ], 10, 3 );
 
 		// Update terms.
 		add_action( 'create_term', [ $this, 'update_term' ], 10, 3 );
@@ -37,6 +44,35 @@ final class WordPress {
 		add_action( 'wp_insert_comment', [ $this, 'update_comment' ] );
 		add_action( 'edit_comment', [ $this, 'update_comment' ] );
 		add_action( 'delete_comment', [ $this, 'update_comment' ] );
+
+		add_action( 'wp_set_comment_status', [ $this, 'update_comment_status' ], 10, 2 );
+
+		// Start import.
+		add_action( 'import_start', [ $this, 'start_import' ] );
+	}
+
+	/**
+	 * Updates user.
+	 *
+	 * @param int $user_id User ID.
+	 */
+	public function update_user( $user_id ) {
+
+		// Check import status.
+		if ( $this->is_import_started() ) {
+			return;
+		}
+
+		// Update user.
+		list($model, $action) = explode( '_', current_action() );
+
+		if ( 'register' === $action ) {
+			$action = 'create';
+		} elseif ( 'update' !== $action ) {
+			$action = 'delete';
+		}
+
+		do_action( 'hivepress/v1/models/user/' . $action, $user_id );
 	}
 
 	/**
@@ -47,6 +83,13 @@ final class WordPress {
 	 * @param bool    $update Update flag.
 	 */
 	public function update_post( $post_id, $post, $update ) {
+
+		// Check import status.
+		if ( $this->is_import_started() ) {
+			return;
+		}
+
+		// Update post.
 		$post_type = get_post_type( $post_id );
 
 		if ( strpos( $post_type, 'hp_' ) === 0 ) {
@@ -66,6 +109,27 @@ final class WordPress {
 	}
 
 	/**
+	 * Updates post status.
+	 *
+	 * @param string  $new_status New status.
+	 * @param string  $old_status Old status.
+	 * @param WP_Post $post Post object.
+	 */
+	public function update_post_status( $new_status, $old_status, $post ) {
+
+		// Check import status.
+		if ( $this->is_import_started() ) {
+			return;
+		}
+
+		// Update post status.
+		if ( strpos( $post->post_type, 'hp_' ) === 0 && $new_status !== $old_status ) {
+			do_action( 'hivepress/v1/models/post/update_status', $post_id, $new_status, $old_status );
+			do_action( 'hivepress/v1/models/' . hp\unprefix( $post->post_type ) . '/update_status', $post_id, $new_status, $old_status );
+		}
+	}
+
+	/**
 	 * Updates term.
 	 *
 	 * @param int    $term_id Term ID.
@@ -73,6 +137,13 @@ final class WordPress {
 	 * @param string $taxonomy Taxonomy name.
 	 */
 	public function update_term( $term_id, $term_taxonomy_id, $taxonomy ) {
+
+		// Check import status.
+		if ( $this->is_import_started() ) {
+			return;
+		}
+
+		// Update term.
 		if ( strpos( $taxonomy, 'hp_' ) === 0 ) {
 			list($action, $model) = explode( '_', current_action() );
 
@@ -91,6 +162,13 @@ final class WordPress {
 	 * @param int $comment_id Comment ID.
 	 */
 	public function update_comment( $comment_id ) {
+
+		// Check import status.
+		if ( $this->is_import_started() ) {
+			return;
+		}
+
+		// Update comment.
 		$comment_type = get_comment_type( $comment_id );
 
 		if ( strpos( $comment_type, 'hp_' ) === 0 ) {
@@ -105,5 +183,45 @@ final class WordPress {
 			do_action( 'hivepress/v1/models/comment/' . $action, $comment_id );
 			do_action( 'hivepress/v1/models/' . hp\unprefix( $comment_type ) . '/' . $action, $comment_id );
 		}
+	}
+
+	/**
+	 * Updates comment status.
+	 *
+	 * @param int    $comment_id Comment ID.
+	 * @param string $new_status New status.
+	 */
+	public function update_comment_status( $comment_id, $new_status ) {
+
+		// Check import status.
+		if ( $this->is_import_started() ) {
+			return;
+		}
+
+		// Update comment status.
+		$comment_type = get_comment_type( $comment_id );
+
+		if ( strpos( $comment_type, 'hp_' ) === 0 ) {
+			do_action( 'hivepress/v1/models/comment/update_status', $comment_id, $new_status );
+			do_action( 'hivepress/v1/models/' . hp\unprefix( $comment_type ) . '/update_status', $comment_id, $new_status );
+		}
+	}
+
+	/**
+	 * Starts import.
+	 */
+	public function start_import() {
+		if ( ! defined( 'HP_IMPORT' ) ) {
+			define( 'HP_IMPORT', true );
+		}
+	}
+
+	/**
+	 * Checks import status.
+	 *
+	 * @return bool
+	 */
+	private function is_import_started() {
+		return defined( 'HP_IMPORT' ) && HP_IMPORT;
 	}
 }
