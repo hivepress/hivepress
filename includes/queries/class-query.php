29f7052cf0b1581724b21dc8be7aaf5c1f89e79d
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-29 20:00:28 +0200

    Add abstract methods

--- includes/queries/class-query.php
+++ includes/queries/class-query.php
@@ -112,4 +112,81 @@ abstract class Query {
 
 		return $operator;
 	}
+
+	/**
+	 * Sets object filters.
+	 *
+	 * @param array $args Filter arguments.
+	 * @return object
+	 */
+	abstract public function filter( $args );
+
+	/**
+	 * Sets object order.
+	 *
+	 * @param array $args Order arguments.
+	 * @return object
+	 */
+	abstract public function order( $args );
+
+	/**
+	 * Limits the number of objects.
+	 *
+	 * @param int $number Objects number.
+	 * @return object
+	 */
+	abstract public function limit( $number );
+
+	/**
+	 * Sets the current page number.
+	 *
+	 * @param int $number Page number.
+	 * @return object
+	 */
+	abstract public function paginate( $number );
+
+	/**
+	 * Offsets the number of objects.
+	 *
+	 * @param int $number Objects number.
+	 * @return object
+	 */
+	abstract public function offset( $number );
+
+	/**
+	 * Gets objects.
+	 *
+	 * @return array
+	 */
+	abstract public function get();
+
+	/**
+	 * Gets object IDs.
+	 *
+	 * @return array
+	 */
+	abstract public function get_ids();
+
+	/**
+	 * Gets the first object.
+	 *
+	 * @return mixed
+	 */
+	final public function get_first() {
+		return hp\get_array_value( $this->limit( 1 )->get(), 0 );
+	}
+
+	/**
+	 * Gets the first object ID.
+	 *
+	 * @return mixed
+	 */
+	final public function get_first_id() {
+		return hp\get_array_value( $this->limit( 1 )->get_ids(), 0 );
+	}
+
+	/**
+	 * Deletes objects.
+	 */
+	abstract public function delete();
 }
