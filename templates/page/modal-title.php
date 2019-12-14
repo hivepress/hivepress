cd3de0559e3c8d80cbb7563514f032ef8b5fdfa7
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-12-15 01:29:09 +0200

    Fix context

--- templates/page/modal-title.php
+++ templates/page/modal-title.php
@@ -2,4 +2,4 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<h3 class="hp-modal__title"><?php echo esc_html( $title ); ?></h3>
+<h3 class="hp-modal__title"><?php echo esc_html( $modal_title ); ?></h3>
