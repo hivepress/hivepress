51085918c536bbbc56985a044de41edacaba1860
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-12-16 22:37:11 +0200

    Fix templates

---
+++ includes/templates/class-site-header-block.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Site header block template.
+ *
+ * @template site_header_block
+ * @description Site header block.
+ * @package HivePress\Templates
+ */
+
+namespace HivePress\Templates;
+
+use HivePress\Helpers as hp;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Site header block template class.
+ *
+ * @class Site_Header_Block
+ */
+class Site_Header_Block extends Template {
+
+	/**
+	 * Template blocks.
+	 *
+	 * @var array
+	 */
+	protected static $blocks = [];
+
+	/**
+	 * Class initializer.
+	 *
+	 * @param array $args Template arguments.
+	 */
+	public static function init( $args = [] ) {
+		$args = hp\merge_trees(
+			[
+				'blocks' => [
+					'main_menu' => [
+						'type'       => 'container',
+						'order'      => 10,
+
+						'attributes' => [
+							'class' => [ 'hp-menu', 'hp-menu--main' ],
+						],
+
+						'blocks'     => [
+							'user_account_link'   => [
+								'type'     => 'element',
+								'filepath' => 'user/login/user-login-link',
+								'order'    => 10,
+							],
+
+							'listing_submit_link' => [
+								'type'     => 'element',
+								'filepath' => 'listing/submit/listing-submit-link',
+								'order'    => 20,
+							],
+						],
+					],
+				],
+			],
+			$args,
+			'blocks'
+		);
+
+		parent::init( $args );
+	}
+}
