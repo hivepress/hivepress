8e57ff2d2907a5d4cdabee4fd5e68575be1cbbb9
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-12-18 02:06:02 +0200

    Fix WooCommerce account

--- templates/woocommerce/myaccount/my-account.php
+++ templates/woocommerce/myaccount/my-account.php
@@ -2,4 +2,4 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-echo ( new HivePress\Blocks\Template( [ 'template' => 'account_page' ] ) )->render();
+echo ( new HivePress\Blocks\Template( [ 'template' => 'user_account_page' ] ) )->render();
