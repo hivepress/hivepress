5971291aea681b36d247aa65d1538a974c7382fc
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-12-21 23:35:43 +0200

    Add prefix option

--- includes/traits/class-mutator.php
+++ includes/traits/class-mutator.php
@@ -24,10 +24,13 @@ trait Mutator {
 	 *
 	 * @param string $name Property name.
 	 * @param mixed  $value Property value.
+	 * @param string $prefix Method prefix.
 	 */
-	final protected function set_property( $name, $value ) {
-		if ( method_exists( $this, 'set_' . $name ) && ! ( new \ReflectionMethod( $this, 'set_' . $name ) )->isStatic() ) {
-			call_user_func_array( [ $this, 'set_' . $name ], [ $value ] );
+	final protected function set_property( $name, $value, $prefix = '' ) {
+		$method = $prefix . 'set_' . $name;
+
+		if ( method_exists( $this, $method ) && ! ( new \ReflectionMethod( $this, $method ) )->isStatic() ) {
+			call_user_func_array( [ $this, $method ], [ $value ] );
 		} elseif ( property_exists( $this, $name ) && ! ( new \ReflectionProperty( $this, $name ) )->isStatic() ) {
 			$this->$name = $value;
 		}
@@ -38,10 +41,13 @@ trait Mutator {
 	 *
 	 * @param string $name Property name.
 	 * @param mixed  $value Property value.
+	 * @param string $prefix Method prefix.
 	 */
-	final protected static function set_static_property( $name, $value ) {
-		if ( method_exists( static::class, 'set_' . $name ) && ( new \ReflectionMethod( static::class, 'set_' . $name ) )->isStatic() ) {
-			call_user_func_array( [ static::class, 'set_' . $name ], [ $value ] );
+	final protected static function set_static_property( $name, $value, $prefix = '' ) {
+		$method = $prefix . 'set_' . $name;
+
+		if ( method_exists( static::class, $method ) && ( new \ReflectionMethod( static::class, $method ) )->isStatic() ) {
+			call_user_func_array( [ static::class, $method ], [ $value ] );
 		} elseif ( property_exists( static::class, $name ) && ( new \ReflectionProperty( static::class, $name ) )->isStatic() ) {
 			static::$$name = $value;
 		}
