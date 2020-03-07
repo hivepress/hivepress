ee1a06e36f743aa061d805edef1bd91353a18070
Ihor <ihor@hivepress.io>
2020-03-07 14:42:00 +0200

    Add vendor category

--- includes/configs/taxonomies.php
+++ includes/configs/taxonomies.php
@@ -29,4 +29,23 @@ return [
 			'not_found'     => esc_html__( 'No categories found.', 'hivepress' ),
 		],
 	],
+
+	'vendor_category'  => [
+		'post_type'         => [ 'vendor', 'vendor_attribute' ],
+		'hierarchical'      => true,
+		'show_admin_column' => true,
+		'rewrite'           => [ 'slug' => 'vendor-category' ],
+
+		'labels'            => [
+			'name'          => esc_html__( 'Categories', 'hivepress' ),
+			'singular_name' => esc_html__( 'Category', 'hivepress' ),
+			'add_new_item'  => esc_html__( 'Add Category', 'hivepress' ),
+			'edit_item'     => esc_html__( 'Edit Category', 'hivepress' ),
+			'update_item'   => esc_html__( 'Update Category', 'hivepress' ),
+			'view_item'     => esc_html__( 'View Category', 'hivepress' ),
+			'parent_item'   => esc_html__( 'Parent Category', 'hivepress' ),
+			'search_items'  => esc_html__( 'Search Categories', 'hivepress' ),
+			'not_found'     => esc_html__( 'No categories found.', 'hivepress' ),
+		],
+	],
 ];
