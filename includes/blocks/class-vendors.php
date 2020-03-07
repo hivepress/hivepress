edb91898400165b1b751141477b92c1594acc98c
Ihor <ihor@hivepress.io>
2020-03-07 14:39:52 +0200

    Fix vendor blocks

--- includes/blocks/class-vendors.php
+++ includes/blocks/class-vendors.php
@@ -21,31 +21,145 @@ defined( 'ABSPATH' ) || exit;
 class Vendors extends Block {
 
 	/**
+	 * Columns number.
+	 *
+	 * @var int
+	 */
+	protected $columns;
+
+	/**
+	 * Vendors number.
+	 *
+	 * @var int
+	 */
+	protected $number;
+
+	/**
+	 * Vendors order.
+	 *
+	 * @var string
+	 */
+	protected $order;
+
+	/**
+	 * Class initializer.
+	 *
+	 * @param array $meta Block meta.
+	 */
+	public static function init( $meta = [] ) {
+		$meta = hp\merge_arrays(
+			[
+				'label'    => hivepress()->translator->get_string( 'vendors' ),
+
+				'settings' => [
+					'columns' => [
+						'label'    => esc_html_x( 'Columns', 'quantity', 'hivepress' ),
+						'type'     => 'select',
+						'default'  => 3,
+						'required' => true,
+						'_order'   => 10,
+
+						'options'  => [
+							2 => '2',
+							3 => '3',
+							4 => '4',
+						],
+					],
+
+					'number'  => [
+						'label'     => esc_html_x( 'Number', 'quantity', 'hivepress' ),
+						'type'      => 'number',
+						'min_value' => 1,
+						'default'   => 3,
+						'required'  => true,
+						'_order'    => 20,
+					],
+
+					'order'   => [
+						'label'    => esc_html_x( 'Order', 'sort', 'hivepress' ),
+						'type'     => 'select',
+						'required' => true,
+						'_order'   => 30,
+
+						'options'  => [
+							'registered_date' => esc_html_x( 'Date Registered', 'sort order', 'hivepress' ),
+							'name'            => esc_html_x( 'Name', 'sort order', 'hivepress' ),
+							'random'          => esc_html_x( 'Random', 'sort order', 'hivepress' ),
+						],
+					],
+				],
+			],
+			$meta
+		);
+
+		parent::init( $meta );
+	}
+
+	/**
 	 * Renders block HTML.
 	 *
 	 * @return string
 	 */
 	public function render() {
+		global $wp_query;
+
 		$output = '';
 
-		// Set query.
-		$query = Models\Vendor::query()->filter( [ 'status' => 'publish' ] )->limit( 1 );
+		// Get column width.
+		$columns      = absint( $this->columns );
+		$column_width = 12;
 
-		// Set listing ID.
-		$listing = $this->get_context( 'listing' );
-
-		if ( hp\is_class_instance( $listing, '\HivePress\Models\Listing' ) ) {
-			$query->filter( [ 'id__in' => [ $listing->get_vendor__id() ] ] );
+		if ( $columns > 0 && $columns <= 12 ) {
+			$column_width = round( $column_width / $columns );
 		}
 
-		// Query vendors.
-		$regular_query = new \WP_Query( $query->get_args() );
+		// Get vendor query.
+		$regular_query = $wp_query;
 
-		// Render vendors.
+		if ( ! isset( $this->context['vendors'] ) ) {
+
+			// Set query.
+			$query = Models\Vendor::query()->filter( [ 'status' => 'publish' ] )->limit( $this->number );
+
+			// Set order.
+			if ( 'name' === $this->order ) {
+				$query->order( [ 'name' => 'asc' ] );
+			} elseif ( 'random' === $this->order ) {
+				$query->order( 'random' );
+			} else {
+				$query->order( [ 'registered_date' => 'desc' ] );
+			}
+
+			// Get cached IDs.
+			$vendor_ids = null;
+
+			if ( 'random' !== $this->order ) {
+				$vendor_ids = hivepress()->cache->get_cache( array_merge( $query->get_args(), [ 'fields' => 'ids' ] ), 'models/vendor' );
+
+				if ( is_array( $vendor_ids ) ) {
+					$query = Models\Vendor::query()->filter(
+						[
+							'status' => 'publish',
+							'id__in' => $vendor_ids,
+						]
+					)->order( 'id__in' )->limit( count( $vendor_ids ) );
+				}
+			}
+
+			// Query vendors.
+			$regular_query = new \WP_Query( $query->get_args() );
+
+			// Cache IDs.
+			if ( 'random' !== $this->order && is_null( $vendor_ids ) && $regular_query->post_count <= 1000 ) {
+				hivepress()->cache->set_cache( array_merge( $query->get_args(), [ 'fields' => 'ids' ] ), 'models/vendor', wp_list_pluck( $regular_query->posts, 'ID' ) );
+			}
+		}
+
 		if ( $regular_query->have_posts() ) {
-			$output  = '<div class="hp-grid">';
+			$output .= '<div class="hp-grid hp-block">';
 			$output .= '<div class="hp-row">';
 
+			// Render vendors.
 			while ( $regular_query->have_posts() ) {
 				$regular_query->the_post();
 
@@ -53,7 +167,7 @@ class Vendors extends Block {
 				$vendor = Models\Vendor::query()->get_by_id( get_post() );
 
 				if ( $vendor ) {
-					$output .= '<div class="hp-grid__item hp-col-xs-12">';
+					$output .= '<div class="hp-grid__item hp-col-sm-' . esc_attr( $column_width ) . ' hp-col-xs-12">';
 
 					// Render vendor.
 					$output .= ( new Template(
