fcb72fb3c4abfaafa1221465e51b3f7f69813fa3
Ihor <ihor@hivepress.io>
2020-03-07 14:39:08 +0200

    Add vendor forms

---
+++ includes/forms/class-vendor-search.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Vendor search form.
+ *
+ * @package HivePress\Forms
+ */
+
+namespace HivePress\Forms;
+
+use HivePress\Helpers as hp;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Vendor search form class.
+ *
+ * @class Vendor_Search
+ */
+class Vendor_Search extends Form {
+
+	/**
+	 * Class initializer.
+	 *
+	 * @param array $meta Form meta.
+	 */
+	public static function init( $meta = [] ) {
+		$meta = hp\merge_arrays(
+			[
+				'model' => 'vendor',
+			],
+			$meta
+		);
+
+		parent::init( $meta );
+	}
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $args Form arguments.
+	 */
+	public function __construct( $args = [] ) {
+		$args = hp\merge_arrays(
+			[
+				'action' => home_url( '/' ),
+				'method' => 'GET',
+
+				'fields' => [
+					's'         => [
+						'placeholder'  => esc_html__( 'Keywords', 'hivepress' ),
+						'type'         => 'text',
+						'display_type' => 'search',
+						'max_length'   => 256,
+						'_order'       => 10,
+					],
+
+					'post_type' => [
+						'type'    => 'hidden',
+						'default' => 'hp_vendor',
+					],
+				],
+
+				'button' => [
+					'label' => esc_html__( 'Search', 'hivepress' ),
+				],
+			],
+			$args
+		);
+
+		parent::__construct( $args );
+	}
+}
---
+++ includes/forms/class-vendor-sort.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Vendor sort form.
+ *
+ * @package HivePress\Forms
+ */
+
+namespace HivePress\Forms;
+
+use HivePress\Helpers as hp;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Vendor sort form class.
+ *
+ * @class Vendor_Sort
+ */
+class Vendor_Sort extends Form {
+
+	/**
+	 * Class initializer.
+	 *
+	 * @param array $meta Form meta.
+	 */
+	public static function init( $meta = [] ) {
+		$meta = hp\merge_arrays(
+			[
+				'model' => 'vendor',
+			],
+			$meta
+		);
+
+		parent::init( $meta );
+	}
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $args Form arguments.
+	 */
+	public function __construct( $args = [] ) {
+		$args = hp\merge_arrays(
+			[
+				'action' => home_url( '/' ),
+				'method' => 'GET',
+				'button' => null,
+
+				'fields' => [
+					'_sort'     => [
+						'label'       => esc_html__( 'Sort by', 'hivepress' ),
+						'type'        => 'select',
+						'placeholder' => null,
+						'options'     => [],
+						'required'    => true,
+						'_order'      => 10,
+					],
+
+					'_category' => [
+						'type' => 'hidden',
+					],
+
+					's'         => [
+						'type' => 'hidden',
+					],
+
+					'post_type' => [
+						'type'    => 'hidden',
+						'default' => 'hp_vendor',
+					],
+				],
+			],
+			$args
+		);
+
+		parent::__construct( $args );
+	}
+
+	/**
+	 * Bootstraps form properties.
+	 */
+	protected function boot() {
+
+		// Set attributes.
+		$this->attributes = hp\merge_arrays(
+			$this->attributes,
+			[
+				'data-autosubmit' => 'true',
+			]
+		);
+
+		parent::boot();
+	}
+}
