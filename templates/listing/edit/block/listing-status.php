3c5fce01894210e8d53938f688e98b774f7b4599
Ihor <ihor@hivepress.io>
2020-05-09 20:07:55 +0300

    Add common styles for statuses

--- templates/listing/edit/block/listing-status.php
+++ templates/listing/edit/block/listing-status.php
@@ -2,7 +2,7 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<td class="hp-listing__status hp-listing__status--<?php echo esc_attr( $listing->get_status() ); ?>">
+<td class="hp-listing__status hp-status hp-status--<?php echo esc_attr( $listing->get_status() ); ?>">
 	<?php if ( $listing->display_status() ) : ?>
 		<span><?php echo esc_html( $listing->display_status() ); ?></span>
 	<?php endif; ?>
