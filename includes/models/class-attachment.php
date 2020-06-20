772c90f57ebdf7859f7d8d2cc0e080058d997f0b
Ihor <ihor@hivepress.io>
2020-06-20 13:36:53 +0300

    Revert "Add attachment title and URL"
    This reverts commit b1e75b0c81e26fd114ac63d53bae954188948bb5.

--- includes/models/class-attachment.php
+++ includes/models/class-attachment.php
@@ -44,17 +44,6 @@ class Attachment extends Post {
 		$args = hp\merge_arrays(
 			[
 				'fields' => [
-					'title'        => [
-						'type'       => 'text',
-						'max_length' => 256,
-						'_alias'     => 'post_title',
-					],
-
-					'url'          => [
-						'type'   => 'url',
-						'_alias' => 'guid',
-					],
-
 					'mime_type'    => [
 						'type'       => 'text',
 						'max_length' => 256,
