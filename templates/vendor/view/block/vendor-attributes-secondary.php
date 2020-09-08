54dc46f3258206e0f21dc6899c846617223ba61d
Ihor <ihor@hivepress.io>
2020-09-09 00:08:15 +0300

    Fix attribute display format

--- templates/vendor/view/block/vendor-attributes-secondary.php
+++ templates/vendor/view/block/vendor-attributes-secondary.php
@@ -12,8 +12,7 @@ if ( $vendor->_get_fields( 'view_block_secondary' ) ) :
 					?>
 					<div class="hp-col-lg-6 hp-col-xs-12">
 						<div class="hp-vendor__attribute hp-vendor__attribute--<?php echo esc_attr( hivepress()->helper->sanitize_slug( $field->get_name() ) ); ?>">
-							<strong><?php echo esc_html( $field->get_label() ); ?>:</strong>
-							<span><?php echo $field->display(); ?></span>
+							<?php echo $field->display(); ?>
 						</div>
 					</div>
 					<?php
