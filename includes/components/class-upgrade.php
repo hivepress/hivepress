22e7df6d26011285721a456e949e30b6c2e6c4b2
Ihor <ihor@hivepress.io>
2020-09-09 18:55:24 +0300

    Fix upgrading attributes

--- includes/components/class-upgrade.php
+++ includes/components/class-upgrade.php
@@ -198,13 +198,13 @@ final class Upgrade extends Component {
 
 				if ( array_intersect( [ 'view_block_secondary', 'view_page_secondary' ], $areas ) && strpos( $format, '%label%' ) === false ) {
 					update_post_meta( $attribute->ID, 'hp_display_format', '%label%: ' . $format );
-				}
 
-				wp_update_post(
-					[
-						'ID' => $attribute->ID,
-					]
-				);
+					wp_update_post(
+						[
+							'ID' => $attribute->ID,
+						]
+					);
+				}
 			}
 		}
 	}
