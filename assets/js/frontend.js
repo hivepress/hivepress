4232f416ccafc40363e281837d2edbf39643085b
Ihor <ihor@hivepress.io>
2020-12-19 02:01:57 +0200

    Fix cached form buttons

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -103,6 +103,15 @@
 			});
 		});
 
+		$(window).on('pageshow', function(e) {
+			if (e.originalEvent.persisted) {
+				var buttons = $('input[type=submit], button[type=submit]');
+
+				buttons.prop('disabled', false);
+				buttons.attr('data-state', '');
+			}
+		});
+
 		// Toggle
 		hivepress.getComponent('toggle').each(function() {
 			var button = $(this);
