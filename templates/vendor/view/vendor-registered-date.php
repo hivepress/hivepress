e70cc5c7f9b9d8e8a8c8b9cc1c9c1e3f85aed2c9
Ihor <ihor@hivepress.io>
2021-01-27 13:16:23 +0200

    Fix templates

--- templates/vendor/view/vendor-registered-date.php
+++ templates/vendor/view/vendor-registered-date.php
@@ -2,7 +2,7 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<time class="hp-vendor__registered-date hp-vendor__date" datetime="<?php echo esc_attr( $vendor->get_registered_date() ); ?>">
+<time class="hp-vendor__registered-date hp-vendor__date hp-meta" datetime="<?php echo esc_attr( $vendor->get_registered_date() ); ?>">
 	<?php
 	/* translators: %s: date. */
 	printf( esc_html__( 'Member since %s', 'hivepress' ), $vendor->display_registered_date() );
