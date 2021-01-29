ee84e213599f87666836004b20a0e51e8a7d0ac5
Ihor <ihor@hivepress.io>
2021-01-29 17:42:01 +0200

    Add image size settings

--- includes/configs/image-sizes.php
+++ includes/configs/image-sizes.php
@@ -12,18 +12,21 @@ defined( 'ABSPATH' ) || exit;
 
 return [
 	'square_small'    => [
+		'label'  => esc_html__( 'Square size', 'hivepress' ) . ' ' . sprintf( '(%s)', esc_html_x( 'small', 'size', 'hivepress' ) ),
 		'width'  => 400,
 		'height' => 400,
 		'crop'   => true,
 	],
 
 	'landscape_small' => [
+		'label'  => esc_html__( 'Landscape size', 'hivepress' ) . ' ' . sprintf( '(%s)', esc_html_x( 'small', 'size', 'hivepress' ) ),
 		'width'  => 400,
 		'height' => 300,
 		'crop'   => true,
 	],
 
 	'landscape_large' => [
+		'label'  => esc_html__( 'Landscape size', 'hivepress' ) . ' ' . sprintf( '(%s)', esc_html_x( 'large', 'size', 'hivepress' ) ),
 		'width'  => 800,
 		'height' => 600,
 		'crop'   => true,
