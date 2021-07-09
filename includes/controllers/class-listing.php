f66bf993e6e01f6ee62d27c4b709dc12d349f501
Ihor <ihor@hivepress.io>
2021-07-09 21:49:08 +0300

    Add tokens to admin emails

--- includes/controllers/class-listing.php
+++ includes/controllers/class-listing.php
@@ -329,6 +329,8 @@ final class Listing extends Controller {
 						'listing_title'      => $listing->get_title(),
 						'listing_attributes' => implode( ', ', $attributes ),
 						'listing_url'        => hivepress()->router->get_admin_url( 'post', $listing->get_id() ),
+						'listing'            => $listing,
+						'user'               => hivepress()->request->get_user(),
 					],
 				]
 			) )->send();
@@ -439,6 +441,8 @@ final class Listing extends Controller {
 					'listing_title'  => $listing->get_title(),
 					'listing_url'    => get_permalink( $listing->get_id() ),
 					'report_details' => $form->get_value( 'details' ),
+					'listing'        => $listing,
+					'user'           => hivepress()->request->get_user(),
 				],
 			]
 		) )->send();
@@ -1020,6 +1024,8 @@ final class Listing extends Controller {
 				'tokens'    => [
 					'listing_title' => $listing->get_title(),
 					'listing_url'   => 'publish' === $status ? get_permalink( $listing->get_id() ) : get_preview_post_link( $listing->get_id() ),
+					'listing'       => $listing,
+					'user'          => hivepress()->request->get_user(),
 				],
 			]
 		) )->send();
