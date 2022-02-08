578fbec3ae6fd463f013c60b17ee470d049183bc
CoolAppCreation <kuznietevhen@gmail.com>
2022-02-08 20:22:15 +0200

    Detect modals on backend forms

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -95,5 +95,10 @@
 			this.select();
 			document.execCommand('copy');
 		});
+
+		// Detect modal.
+        $('form').each(function() {
+            $(this).find('[data-component="modal"]').detach().insertAfter(this);
+        });
 	});
 })(jQuery);
