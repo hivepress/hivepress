3826b3acf5851196549a0f2b99454fb33fee7282
ihor <ihor@hivepress.io>
2022-03-25 20:45:09 +0200

    Add block labels

--- includes/templates/class-page-narrow.php
+++ includes/templates/class-page-narrow.php
@@ -59,6 +59,7 @@ abstract class Page_Narrow extends Page {
 											'page_title'  => [
 												'type'   => 'part',
 												'path'   => 'page/page-title',
+												'_label' => esc_html__( 'Page Title', 'hivepress' ),
 												'_order' => 5,
 											],
 
