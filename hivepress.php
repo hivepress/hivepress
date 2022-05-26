ef271f9b6eca8a02775da21a53a37b0884aa1d2c
ihor <ihor@hivepress.io>
2022-05-26 15:41:07 +0300

    Update version

--- hivepress.php
+++ hivepress.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: HivePress
  * Description: Multipurpose directory, listing & classifieds plugin.
- * Version: 1.6.6
+ * Version: 1.6.7
  * Author: HivePress
  * Author URI: https://hivepress.io/
  * Text Domain: hivepress
