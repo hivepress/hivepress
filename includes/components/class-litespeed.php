49485bb168c7ab96dffe81e1053aa15939a8aac7
kuznev <kuznietevhen@gmail.com>
2022-05-31 17:41:48 +0300

    Disable LiteSpeed cache for logged-in users

--- includes/components/class-litespeed.php
+++ includes/components/class-litespeed.php
@@ -32,6 +32,9 @@ final class LiteSpeed extends Component {
 		// Disable login cache.
 		add_action( 'hivepress/v1/models/user/login', [ $this, 'disable_login_cache' ] );
 
+		// Disable cache for logged in users.
+		add_action( 'litespeed_init', [ $this, 'disable_logged_in_cache' ] );
+
 		parent::__construct( $args );
 	}
 
@@ -40,5 +43,16 @@ final class LiteSpeed extends Component {
 	 */
 	public function disable_login_cache() {
 		add_filter( 'litespeed_can_change_vary', '__return_true' );
+	}
+
+	/**
+	 * Disables cache for logged in users.
+	 */
+	public function disable_logged_in_cache() {
+		if ( get_option( 'litespeed.conf.cache-priv' ) ) {
+			update_option( 'litespeed.conf.cache-priv', null );
+			do_action( 'litespeed_conf_force', 'cache-priv', false );
+			do_action( 'litespeed_update_confs', 'cache-priv' );
+		}
 	}
 }
