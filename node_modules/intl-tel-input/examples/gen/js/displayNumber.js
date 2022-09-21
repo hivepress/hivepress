f42812fc1a74050bcb3372871c9ba7142e1c2ccd
kuznev <kuznietevhen@gmail.com>
2022-09-21 17:35:10 +0300

    Revert changes

--- node_modules/intl-tel-input/examples/gen/js/displayNumber.js
+++
@@ -1,4 +0,0 @@
-var input = document.querySelector("#phone");
-window.intlTelInput(input, {
-  utilsScript: "../../build/js/utils.js?1638200991544" // just for formatting/placeholders etc
-});
--- node_modules/intl-tel-input/examples/gen/js/initPromise.js
+++
@@ -1,9 +0,0 @@
-var input = document.querySelector("#phone");
-var statusElement = document.querySelector("#status");
-
-var iti = window.intlTelInput(input, {
-  utilsScript: "../../build/js/utils.js?1638200991544",
-});
-iti.promise.then(function() {
-  statusElement.innerHTML = "Initialised!";
-});
