a5da82809da06538df12d8e142ce0dceba635893
Vasiliy Kryukov <mr.destroyko@gmail.com>
2022-10-26 10:09:45 +0300

    create/config email

---
+++ includes/emails/class-user-delete.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * User register email.
+ *
+ * @package HivePress\Emails
+ */
+
+namespace HivePress\Emails;
+
+use HivePress\Helpers as hp;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Sent to users on account registration.
+ */
+class User_Delete extends Email {
+
+	/**
+	 * Class initializer.
+	 *
+	 * @param array $meta Class meta values.
+	 */
+	public static function init( $meta = [] ) {
+		$meta = hp\merge_arrays(
+			[
+				'label'       => esc_html__( 'User Deleted', 'hivepress' ),
+				'description' => esc_html__( 'This email is sent to users after deleting account.', 'hivepress' ),
+				'recipient'   => hivepress()->translator->get_string( 'user' ),
+				'tokens'      => [ 'user_name', 'user' ],
+			],
+			$meta
+		);
+
+		parent::init( $meta );
+	}
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $args Email arguments.
+	 */
+	public function __construct( $args = [] ) {
+		$args = hp\merge_arrays(
+			[
+				'subject' => esc_html__( 'Delete account complete', 'hivepress' ),
+				'body'    => hp\sanitize_html( __( "Hi, %user_name%! Your account and personal data were deleted. Thank you for using our services.", 'hivepress' ) ),
+			],
+			$args
+		);
+
+		parent::__construct( $args );
+	}
+}
