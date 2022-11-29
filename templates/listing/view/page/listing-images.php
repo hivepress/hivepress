a84895dc24f13536c0616a20fa12f67bb660167b
HivePress <43493187+hivepress@users.noreply.github.com>
2022-11-30 00:10:11 +0200

    Fix listing images

--- templates/listing/view/page/listing-images.php
+++ templates/listing/view/page/listing-images.php
@@ -1,12 +1,30 @@
 <?php
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-?>
+
+if ( $listing->get_images__id() ) :
+	$image_urls = [];
+
+	if ( get_option( 'hp_listing_enable_image_zoom' ) ) :
+		$image_urls = $listing->get_images__url( 'large' );
+	endif;
+	?>
 	<div class="hp-listing__images" data-component="carousel-slider">
-		<?php foreach ( $listing->get_images__object() as $image ) :
-			if ( strpos( $image->get_mime_type(), 'image' ) !== false ) : ?>
-				<img src="<?php echo esc_url( $image->get_url( 'hp_landscape_large' ) ); ?>" data-src="<?php echo get_option( 'hp_listing_enable_image_zoom' ) ? esc_url( $image->get_url( 'large' ) ) : null; ?>" alt="<?php echo esc_attr( $listing->get_title() ); ?>" loading="lazy">
-		<?php elseif ( strpos( $image->get_mime_type(), 'video' ) !== false ) : ?>
-			<video data-src="<?php echo get_option( 'hp_listing_enable_image_zoom' ) ? esc_url( $image->get_url() ) : null; ?>" controls><source src="<?php echo esc_url( $image->get_url() ); ?>" type="<?php echo esc_html( $image->get_mime_type() ); ?>"></video>
-			<?php endif; endforeach; ?>
+		<?php
+		foreach ( $listing->get_images() as $image_index => $image ) :
+			$image_url = hivepress()->helper->get_array_value( $image_urls, $image_index );
+
+			if ( strpos( $image->get_mime_type(), 'video' ) === 0 ) :
+				?>
+				<video data-src="<?php echo esc_url( $image_url ); ?>" controls>
+					<source src="<?php echo esc_url( $image->get_url() ); ?>" type="<?php echo esc_attr( $image->get_mime_type() ); ?>">
+				</video>
+			<?php else : ?>
+				<img src="<?php echo esc_url( $image->get_url( 'hp_landscape_large' ) ); ?>" data-src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $listing->get_title() ); ?>" loading="lazy">
+				<?php
+			endif;
+		endforeach;
+		?>
 	</div>
+	<?php
+endif;
