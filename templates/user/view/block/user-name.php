4336ba2c16434de66c0caee8ee98d052c1f37397
HivePress <43493187+hivepress@users.noreply.github.com>
2023-02-21 22:32:22 +0100

    Fix formatting

--- templates/user/view/block/user-name.php
+++ templates/user/view/block/user-name.php
@@ -3,6 +3,7 @@
 defined( 'ABSPATH' ) || exit;
 
 $display = get_option( 'hp_user_enable_display' );
+
 if ( $display ) : ?>
 	<a href="<?php echo esc_url( hivepress()->router->get_url( 'user_view_page', [ 'username' => $user->get_username() ] ) ); ?>">
 	<?php
@@ -13,4 +14,5 @@ echo esc_html( $user->get_display_name() );
 if ( $display ) :
 	?>
 	</a>
-<?php endif; ?>
+	<?php
+endif;
