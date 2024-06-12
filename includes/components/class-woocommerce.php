359a7b6415e1fce5dd880e7f761eb39875aa33f0
Pavlo Alokhin <pnoxville@gmail.com>
2024-06-12 13:29:03 +0300

    Added HPOS disabling with HivePress activation

--- includes/components/class-woocommerce.php
+++ includes/components/class-woocommerce.php
@@ -89,6 +89,10 @@ final class WooCommerce extends Component {
 		update_option( 'woocommerce_enable_guest_checkout', 'no' );
 		update_option( 'woocommerce_enable_checkout_login_reminder', 'yes' );
 		update_option( 'woocommerce_enable_signup_and_login_from_checkout', 'yes' );
+
+		// @todo Remove after HPOS integration.
+		update_option( 'woocommerce_custom_orders_table_enabled', 'no' );
+		update_option( 'woocommerce_custom_orders_table_data_sync_enabled', 'no' );
 	}
 
 	/**
