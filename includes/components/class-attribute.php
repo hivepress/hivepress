619451eb3f517e3faf04b03e422274410536c99d
Pavlo Alokhin <pnoxville@gmail.com>
2024-06-24 15:54:51 +0300

    Added check for model status for a post-based models before applying default attribute values

--- includes/components/class-attribute.php
+++ includes/components/class-attribute.php
@@ -995,9 +995,18 @@ final class Attribute extends Component {
 
 		// Set attributes default values.
 		foreach ( $model->_get_fields() as $field) {
-			if ( ! $field->get_value() && $field->get_default() ) {
-				$field->set_value( $field->get_default() );
+
+			// Check field value.
+			if ( $field->get_value() || ! $field->get_default() ) {
+				continue;
 			}
+
+			// Check model status.
+			if ( in_array( $model::get_meta( 'name' ), $this->get_models( 'post' ), true ) && ! in_array( $model->get_status(), [ 'draft', 'auto-draft' ], true ) ) {
+				continue;
+			}
+
+			$field->set_value( $field->get_default() );
 		}
 
 		return $form_args;
