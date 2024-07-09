3dcc8bcb4b09c19a2c9ddd72207d12e9c3175711
Pavlo Alokhin <pnoxville@gmail.com>
2024-07-09 18:26:57 +0300

    minor fix

--- templates/user/view/block/user-name.php
+++ templates/user/view/block/user-name.php
@@ -6,7 +6,7 @@ $display = get_option( 'hp_user_enable_display' );
 $online_users = (array) get_transient( 'online_users' );
 ?>
 
-<span class="user-<?php echo in_array( $user->get_id(), array_keys( $online_users ) ) && $online_users[ $user->get_id() ] > time() - 60 ? 'online' : 'offline'; ?>"></span>
+<span class="hp-vendor__online-status <?php echo in_array( $user->get_id(), array_keys( $online_users ) ) && $online_users[ $user->get_id() ] > time() - 60 ? 'online' : 'offline'; ?>"></span>
 
 <?php
 if ( $display ) : ?>
--- templates/user/view/page/user-name.php
+++ templates/user/view/page/user-name.php
@@ -4,5 +4,5 @@ defined( 'ABSPATH' ) || exit;
 
 $online_users = (array) get_transient( 'online_users' );
 ?>
-<span class="user-<?php echo in_array( $user->get_id(), array_keys( $online_users ) ) && $online_users[ $user->get_id() ] > time() - 60 ? 'online' : 'offline'; ?>"></span>
+<span class="hp-vendor__online-status <?php echo in_array( $user->get_id(), array_keys( $online_users ) ) && $online_users[ $user->get_id() ] > time() - 60 ? 'online' : 'offline'; ?>"></span>
 <span><?php echo esc_html( $user->get_display_name() ); ?></span>
