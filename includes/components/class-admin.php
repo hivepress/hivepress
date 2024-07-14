7f10ea62abfaa34eddee8854f689e8af10020a09
kuznev <kuznietevhen@gmail.com>
2024-07-14 17:55:36 +0300

    Fix installation extension status

--- includes/components/class-admin.php
+++ includes/components/class-admin.php
@@ -908,7 +908,7 @@ final class Admin extends Component {
 			$extension_status = install_plugin_install_status( $extension );
 
 			// Set activation status.
-			if ( ! in_array( $extension_status['status'], [ 'install', 'update_available' ], true ) && ! is_plugin_active( $extension_path ) ) {
+			if ( $extension_status['file'] && ! is_plugin_active( $extension_path ) ) {
 				$extension_status = array_merge(
 					$extension_status,
 					[
