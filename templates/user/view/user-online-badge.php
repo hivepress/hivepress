e485726edb74eb3c37985faf0a7dd425331eb3b0
HivePress <43493187+hivepress@users.noreply.github.com>
2024-09-11 13:49:40 +0200

    Fix formatting

--- templates/user/view/user-online-badge.php
+++ templates/user/view/user-online-badge.php
@@ -2,8 +2,4 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<span class="hp-vendor__online-badge 
-<?php
-if ( $user_online ) :
-	?>
-	hp-vendor__online-badge--active<?php endif; ?>" title="<?php echo esc_attr( $user_online_status ); ?>"></span>
+<span class="hp-vendor__online-badge <?php if ( $user_online ) : ?>hp-vendor__online-badge--active<?php endif; ?>" title="<?php echo esc_attr( $user_online_status ); ?>"></span>
