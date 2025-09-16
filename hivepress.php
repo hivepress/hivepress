7bc11f03b06a8630ea749906285cdd27af6ede86
HivePress <43493187+hivepress@users.noreply.github.com>
2025-09-16 11:43:49 +0200

    Update version

--- hivepress.php
+++ hivepress.php
@@ -3,7 +3,7 @@
  * Plugin Name: HivePress
  * Plugin URI: https://hivepress.io/
  * Description: Multipurpose directory, listing & classifieds plugin.
- * Version: 1.7.13
+ * Version: 1.7.14
  * Author: HivePress
  * Author URI: https://hivepress.io/
  * Text Domain: hivepress
