92d70dfcf2f62442358b5eb57defd8f682f1c925
HivePress <43493187+hivepress@users.noreply.github.com>
2026-07-01 20:46:40 +0200

    Revert "Fix using quotes in passwords"
    This reverts commit e955b46ed7e48e7d02ff05720cba46fecdb043af.

--- includes/controllers/class-user.php
+++ includes/controllers/class-user.php
@@ -406,7 +406,7 @@ final class User extends Controller {
 			wp_signon(
 				[
 					'user_login'    => $user->get_username(),
-					'user_password' => wp_slash( $form->get_value( 'password' ) ),
+					'user_password' => $form->get_value( 'password' ),
 					'remember'      => true,
 				]
 			);
@@ -455,11 +455,8 @@ final class User extends Controller {
 
 		$user = Models\User::query()->get_by_id( $user_object );
 
-		// Get password.
-		$password = wp_slash( $form->get_value( 'password' ) );
-
 		// Check password.
-		if ( ! wp_check_password( $password, $user->get_password(), $user->get_id() ) ) {
+		if ( ! wp_check_password( $form->get_value( 'password' ), $user->get_password(), $user->get_id() ) ) {
 			return hp\rest_error( 401, esc_html__( 'Username or password is incorrect.', 'hivepress' ) );
 		}
 
@@ -481,7 +478,7 @@ final class User extends Controller {
 			wp_signon(
 				[
 					'user_login'    => $user->get_username(),
-					'user_password' => $password,
+					'user_password' => $form->get_value( 'password' ),
 					'remember'      => true,
 				]
 			);
@@ -605,7 +602,7 @@ final class User extends Controller {
 			wp_signon(
 				[
 					'user_login'    => $user->get_username(),
-					'user_password' => wp_slash( $form->get_value( 'password' ) ),
+					'user_password' => $form->get_value( 'password' ),
 					'remember'      => true,
 				]
 			);
@@ -665,7 +662,7 @@ final class User extends Controller {
 				return hp\rest_error( 400, esc_html__( 'Current password is required.', 'hivepress' ) );
 			}
 
-			if ( ! wp_check_password( wp_slash( $form->get_value( 'current_password' ) ), $user->get_password(), $user->get_id() ) ) {
+			if ( ! wp_check_password( $form->get_value( 'current_password' ), $user->get_password(), $user->get_id() ) ) {
 				return hp\rest_error( 401, esc_html__( 'Current password is incorrect.', 'hivepress' ) );
 			}
 		}
@@ -755,7 +752,7 @@ final class User extends Controller {
 				return hp\rest_error( 400, $form->get_errors() );
 			}
 
-			if ( ! wp_check_password( wp_slash( $form->get_value( 'password' ) ), $user->get_password(), $user->get_id() ) ) {
+			if ( ! wp_check_password( $form->get_value( 'password' ), $user->get_password(), $user->get_id() ) ) {
 				return hp\rest_error( 401, esc_html__( 'Password is incorrect.', 'hivepress' ) );
 			}
 
