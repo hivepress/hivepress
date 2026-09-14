3dd2ab1166369cbd0ff27fbf480159c4bc54ac47
HivePress <43493187+hivepress@users.noreply.github.com>
2026-09-14 19:22:58 +0200

    Add RTL styles replacement

--- includes/components/class-asset.php
+++ includes/components/class-asset.php
@@ -168,19 +168,11 @@ final class Asset extends Component {
 		// Enqueue styles.
 		foreach ( $styles as $style ) {
 
-			// Get version.
-			$version = hp\get_array_value( $style, 'version', hivepress()->get_version() );
-
 			// Enqueue style.
-			wp_enqueue_style( $style['handle'], $style['src'], hp\get_array_value( $style, 'deps', [] ), $version );
+			wp_enqueue_style( $style['handle'], $style['src'], hp\get_array_value( $style, 'deps', [] ), hp\get_array_value( $style, 'version', hivepress()->get_version() ) );
 
-			if ( is_rtl() && hp\get_array_value( $style, 'rtl' ) ) {
-
-				// Get URL.
-				$src = dirname( $style['src'] ) . '/' . preg_replace( '/^([^.]+)/', '$1-rtl', basename( $style['src'] ) );
-
-				// Enqueue RTL style.
-				wp_enqueue_style( $style['handle'] . '-rtl', $src, [ $style['handle'] ], $version );
+			if ( hp\get_array_value( $style, 'rtl' ) ) {
+				wp_style_add_data( $style['handle'], 'rtl', 'replace' );
 			}
 		}
 	}
